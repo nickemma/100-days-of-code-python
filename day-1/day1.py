04116b18f5cbd2fb29c19aebd9ce0dfcab7d4fa0
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-04-15 00:45:58 +0100

    Completed day 1

---
+++ day-1/day1.py
@@ -0,0 +1,11 @@
+print('hello world')
+
+name = input('what is your name? ')
+
+print(len(name))
+
+print('welcome to awesome band name generator')
+city = input('what is the name of the city you grew up? \n')
+pet_name = input('what is the name of your pet? \n')
+
+print('your band name is ' + city + ' ' + pet_name)
