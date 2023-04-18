ff643deeb5670d01d909e5e0062e446d18dca602
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-04-18 01:54:50 +0100

    completed day 5 password generator project

---
+++ baiscs/day5.py
@@ -0,0 +1,55 @@
+# Python Loops
+import random
+
+fruits = ['apple', 'sweet', 'mango']
+
+for fruit in fruits:
+    print(fruit)
+
+student_scores = [74, 87, 91, 56, 76, 98]
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"the highest score is {highest_score}")
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print('fizzbuzz')
+    elif num % 3 == 0:
+        print('fizz')
+    elif num % 5 == 0:
+        print('buzz')
+    else:
+        print(num)
+
+
+# Password generator
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list += random.choice(letters)
+
+for char in range(1, nr_letters + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, nr_letters + 1):
+    password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+password = ''
+for character in password_list:
+    password += character
+print(password)
