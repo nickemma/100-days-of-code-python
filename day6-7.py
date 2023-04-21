0abdaf8d6488b25d1183533bd3f7b6a3067c782c
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-04-21 22:58:30 +0100

    Installed pycharm ide

--- day6-7.py
+++ day6-7.py
@@ -13,7 +13,7 @@ username = input('What is your username? ')
 password = input('What is your password? ')
 
 print('Account created successfully...')
-print('Now login with your credentials')
+print('login with your credentials')
 
 username2 = input('Login with your username?\n')
 password2 = input('Login with your password?\n')
