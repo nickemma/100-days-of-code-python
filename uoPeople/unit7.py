ca3f76b9384f0c20ff37ad8149e200e16fcc899d
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-10-20 13:38:38 +0100

    completed my discussion

--- uoPeople/unit7.py
+++ uoPeople/unit7.py
@@ -1,29 +1,30 @@
-# def invert_courses_dict(student_courses):
-#     inverted_dict = {}
-#   # Iterate through the original dictionary
-#     for student, courses in student_courses.items():
-#       # Iterate through the list of courses for each student
-#       for course in courses:
-#           if course in inverted_dict:
-#               inverted_dict[course].append(student)
-#           else:
-#               inverted_dict[course] = [student]
+def invert_courses_dict(student_courses):
+    inverted_dict = {}
+  # Iterate through the original dictionary
+    for student, courses in student_courses.items():
+        # Iterate through the list of courses for each student
+        for course in courses:
+            if course in inverted_dict:
+                inverted_dict[course].append(student)
+            else:
+                inverted_dict[course] = [student]
 
-#     return inverted_dict
+    return inverted_dict
 
-# # Sample input dictionary
-# student_courses = {
-#     'Stud1': ['CS1101', 'CS2402', 'CS2001'],
-#     'Stud2': ['CS2402', 'CS2001', 'CS1102']
-# }
 
-# # Invert the dictionary
-# inverted_output = invert_courses_dict(student_courses)
-# # Print the original and inverted dictionaries
-# print("Original Dictionary:")
-# print(student_courses)
-# print("\nInverted Dictionary:")
-# print(inverted_output)
+# Sample input dictionary
+student_courses = {
+    'Stud1': ['CS1101', 'CS2402', 'CS2001'],
+    'Stud2': ['CS2402', 'CS2001', 'CS1102']
+}
+
+# Invert the dictionary
+inverted_output = invert_courses_dict(student_courses)
+# Print the original and inverted dictionaries
+print("Original Dictionary:")
+print(student_courses)
+print("\nInverted Dictionary:")
+print(inverted_output)
 
 # Looping over two lists with zip function
 
